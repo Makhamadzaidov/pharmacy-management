9e967b75c9ccc0ae2f6323a0e20986f9ead69571
Пользователь <kamron25bek25@gmail.com>
2022-10-12 10:12:45 +0500

    DrugsController added

--- src/PharmacyAppExam.WebApi/Controllers/DrugsController.cs
+++ src/PharmacyAppExam.WebApi/Controllers/DrugsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PharmacyAppExam.WebApi.Commons.Utils;
+using PharmacyAppExam.WebApi.Interfaces.Services;
 using PharmacyAppExam.WebApi.ViewModels.Drugs;
 
 namespace PharmacyAppExam.WebApi.Controllers
@@ -9,34 +10,40 @@ namespace PharmacyAppExam.WebApi.Controllers
     [ApiController]
     public class DrugsController : ControllerBase
     {
+        private readonly IDrugService _drugService;
+
+        public DrugsController(IDrugService drugService)
+        {
+            _drugService = drugService;
+        }
         [HttpGet]
         public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
         {
-            return Ok();
+            return Ok(await _drugService.GetAllAsync(expression: null, @params));
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateAsync([FromForm]DrugCreateViewModel drugCreateViewModel)
         {
-            return Ok();
+            return Ok(await _drugService.CreateAsync(drugCreateViewModel));
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteAsync(long id)
         {
-            return Ok();
+            return Ok(await _drugService.DeleteAsync(drug => drug.Id == id));
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetAsync(long id)
         {
-            return Ok();
+            return Ok(await _drugService.GetAsync(drug => drug.Id == id));
         }
 
         [HttpPut]
         public async Task<IActionResult> UpdateAsync(long id, [FromForm]DrugCreateViewModel drugCreateViewModel)
         {
-            return Ok();
+            return Ok(await _drugService.UpdateAsync(id, drugCreateViewModel));
         }
     }
 }
