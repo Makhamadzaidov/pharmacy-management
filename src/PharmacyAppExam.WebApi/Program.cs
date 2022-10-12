90115da394fc4e09ccc70d1270012b56f43bfeb8
Пользователь <kamron25bek25@gmail.com>
2022-10-12 11:20:37 +0500

    init

--- src/PharmacyAppExam.WebApi/Program.cs
+++ src/PharmacyAppExam.WebApi/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IFileService, FileService>();
 builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IDrugService, DrugService>();
 builder.Services.AddAutoMapper(typeof(MapperProfile));
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddMemoryCache();
