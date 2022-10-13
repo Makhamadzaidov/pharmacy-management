d81a935e68f4d9a74a8536c84613797c7dea2f28
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-13 15:49:54 +0500

    UserService added

--- src/PharmacyAppExam.WebApi/Services/UserService.cs
+++ src/PharmacyAppExam.WebApi/Services/UserService.cs
@@ -1,17 +1,44 @@
-﻿using PharmacyAppExam.WebApi.Commons.Utils;
+﻿using AutoMapper;
+using PharmacyAppExam.WebApi.Commons.Exceptions;
+using PharmacyAppExam.WebApi.Commons.Utils;
+using PharmacyAppExam.WebApi.DbContexts;
+using PharmacyAppExam.WebApi.Interfaces.IRepositories;
 using PharmacyAppExam.WebApi.Interfaces.Services;
 using PharmacyAppExam.WebApi.Models;
+using PharmacyAppExam.WebApi.Security;
 using PharmacyAppExam.WebApi.ViewModels.Users;
 using System.Linq.Expressions;
-
+using System.Net;
 
 namespace PharmacyAppExam.WebApi.Services
 {
     public class UserService : IUserService
     {
-        public Task<bool> DeleteAsync(Expression<Func<User, bool>> expression)
+        private readonly IUserRepository _userRepository;
+        private readonly AppDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly IFileService _fileService;
+
+        public UserService(IUserRepository userRepository, AppDbContext dbContext, IMapper mapper, IFileService fileService)
         {
-            throw new NotImplementedException();
+            _userRepository = userRepository;
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _fileService = fileService;
+        }
+
+        public async Task<bool> DeleteAsync(Expression<Func<User, bool>> expression)
+        {
+            var user = await _userRepository.GetAsync(expression);
+
+            if (user is null)
+                throw new StatusCodeException(HttpStatusCode.NotFound, "User not found!");
+
+            await _fileService.DeleteImageAsync(user.ImagePath);
+            await _userRepository.DeleteAsync(user);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
         }
 
         public Task<IEnumerable<UserViewModel>> GetAllAsync(Expression<Func<User, bool>>? expression = null, PaginationParams? @params = null)
@@ -19,14 +46,40 @@ namespace PharmacyAppExam.WebApi.Services
             throw new NotImplementedException();
         }
 
-        public Task<UserViewModel?> GetAsync(Expression<Func<User, bool>> expression)
+        public async Task<UserViewModel?> GetAsync(Expression<Func<User, bool>> expression)
         {
-            throw new NotImplementedException();
+            var user = await _userRepository.GetAsync(expression);
+
+            if (user is null)
+                throw new StatusCodeException(HttpStatusCode.NotFound, "User not found!");
+
+            return _mapper.Map<UserViewModel>(user);
         }
 
-        public Task<UserViewModel> UpdateAsync(long id, UserCreateViewModel userCreateViewModel)
+        public async Task<UserViewModel> UpdateAsync(long id, UserCreateViewModel userCreateViewModel)
         {
-            throw new NotImplementedException();
+            var entity = await _userRepository.GetAsync(user => user.Id == id);
+
+            if (entity is null) 
+                throw new StatusCodeException(HttpStatusCode.NotFound, "User not found!");
+
+            await _fileService.DeleteImageAsync(entity.ImagePath);
+            var imagePath = await _fileService.SaveImageAsync(userCreateViewModel.Image);
+            var res = PasswordHasher.Hash(userCreateViewModel.Password);
+            var hash = res.Hash;
+            var salt = res.Salt;
+
+            var userMap = _mapper.Map<User>(userCreateViewModel);
+
+            userMap.Id = entity.Id;
+            userMap.Salt = entity.Salt;
+            userMap.ImagePath = imagePath;
+            userMap.PasswordHash = hash;
+
+            await _userRepository.UpdateAsync(userMap);
+            await _dbContext.SaveChangesAsync();
+
+            return _mapper.Map<UserViewModel>(userMap);
         }
     }
 }
