4494c226da3f3d5179196a23fbef5f5f6a8ac691
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-13 15:56:37 +0500

    UsersController complated

--- src/PharmacyAppExam.WebApi/Controllers/UsersController.cs
+++ src/PharmacyAppExam.WebApi/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PharmacyAppExam.WebApi.Commons.Utils;
+using PharmacyAppExam.WebApi.Interfaces.Services;
 using PharmacyAppExam.WebApi.ViewModels.Users;
 
 namespace PharmacyAppExam.WebApi.Controllers
@@ -9,28 +10,35 @@ namespace PharmacyAppExam.WebApi.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         [HttpGet]
         public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
         {
-            return Ok();
+            return Ok(await _userService.GetAllAsync(expression: null, @params));
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteAsync(long id)
         {
-            return Ok();
+            return Ok(await _userService.DeleteAsync(user => user.Id == id));
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetAsync(long id)
         {
-            return Ok();
+            return Ok(await _userService.GetAsync(user => user.Id == id));
         }
 
         [HttpPut]
-        public async Task<IActionResult> UpdateAsync([FromForm] UserCreateViewModel userCreateViewModel)
+        public async Task<IActionResult> UpdateAsync(int id, [FromForm] UserCreateViewModel userCreateViewModel)
         {
-            return Ok();
+            return Ok(await _userService.UpdateAsync(id, userCreateViewModel));
         }
     }
 }
