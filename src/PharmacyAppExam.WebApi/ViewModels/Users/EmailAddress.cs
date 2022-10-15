63d652026383545c5704e295c3cb81ad8ccdd25a
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-15 19:05:52 +0500

    Added annotations EmailAddress

--- src/PharmacyAppExam.WebApi/ViewModels/Users/EmailAddress.cs
+++ src/PharmacyAppExam.WebApi/ViewModels/Users/EmailAddress.cs
@@ -1,8 +1,13 @@
-﻿namespace PharmacyAppExam.WebApi.ViewModels.Users
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmacyAppExam.WebApi.ViewModels.Users
 {
     public class EmailAddress
     {
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "Code is required")]
         public string Code { get; set; } = String.Empty;
     }
 }
