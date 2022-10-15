cfc18d00fd4fa2560b72fb697909cdc312b88ae2
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-16 01:41:24 +0500

    Added Jwt and Swagger configurations

---
+++ src/PharmacyAppExam.WebApi/Configurations/JwtConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PharmacyAppExam.WebApi.Configurations
+{
+    public static class JwtConfiguration
+    {
+        public static void ConfigureJwtAuthorize(this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = config["Jwt:Issuer"],
+                        ValidAudience = config["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:SecretKey"]))
+                    };
+                });
+        }
+    }
+}
---
+++ src/PharmacyAppExam.WebApi/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace PharmacyAppExam.WebApi.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        public static void ConfigureSwaggerAuthorize(this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pharmacy.WebApi", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "JWT Authorization header using the Bearer scheme. " +
+                                  "Example: \"Authorization: Bearer {token}\"",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+        }
+    }
+}
