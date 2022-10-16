9182dced1a6194da3dcc079838be5ac96ff61bbd
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-16 06:42:54 +0500

    Added annotations to OrderCreateViewModel

--- src/PharmacyAppExam.WebApi/ViewModels/Orders/OrderCreateViewModel.cs
+++ src/PharmacyAppExam.WebApi/ViewModels/Orders/OrderCreateViewModel.cs
@@ -1,15 +1,19 @@
 ﻿using PharmacyAppExam.WebApi.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace PharmacyAppExam.WebApi.ViewModels.Orders
 {
     public class OrderCreateViewModel
     {
+        [Required(ErrorMessage = "Drug Id is reuqired field")]
         public long DrugId { get; set; }
 
+        [Required(ErrorMessage = "Quantity is required field")]
         public int Quantity { get; set; }
 
-        public string? CardNumber { get; set; }
-
+        [Required(ErrorMessage = "Payment type is required field")]
         public PaymentType PaymentType { get; set; }
+
+        public string? CardNumber { get; set; }
     }
 }
