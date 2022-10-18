ac539e3581de7a2699c0652e384c460af70e1071
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-19 00:58:44 +0500

    JsonPropertyName added

--- src/PharmacyAppExam.WebApi/ViewModels/Drugs/DrugCreateViewModel.cs
+++ src/PharmacyAppExam.WebApi/ViewModels/Drugs/DrugCreateViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace PharmacyAppExam.WebApi.ViewModels.Drugs
 {
@@ -6,21 +7,26 @@ namespace PharmacyAppExam.WebApi.ViewModels.Drugs
     {
         [Required(ErrorMessage = "Drug name is required field")]
         [MaxLength(35), MinLength(2)]
+        [JsonPropertyName("name")]
         public string Name { get; set; } = String.Empty;
 
         [Required(ErrorMessage = "Description is required field")]
+        [JsonPropertyName("description")]
         public string Description { get; set; } = String.Empty;
 
 
         [Required(ErrorMessage = "ImageUrl is required field")]
+        [JsonPropertyName("imageurl")]
         public string ImageUrl { get; set; } = String.Empty;
 
 
         [Required(ErrorMessage = "Price is required field")]
+        [JsonPropertyName("price")]
         public int Price { get; set; }
 
 
         [Required(ErrorMessage = "Quantity is required field")]
+        [JsonPropertyName("quantity")]
         public int Quantity { get; set; }
     }
 }
--- src/PharmacyAppExam.WebApi/ViewModels/Drugs/DrugViewModel.cs
+++ src/PharmacyAppExam.WebApi/ViewModels/Drugs/DrugViewModel.cs
@@ -1,17 +1,25 @@
-﻿namespace PharmacyAppExam.WebApi.ViewModels.Drugs
+﻿using System.Text.Json.Serialization;
+
+namespace PharmacyAppExam.WebApi.ViewModels.Drugs
 {
     public class DrugViewModel
     {
+        [JsonPropertyName("id")]
         public long Id { get; set; }
 
+        [JsonPropertyName("name")]
         public string Name { get; set; } = String.Empty;
 
+        [JsonPropertyName("description")]
         public string Description { get; set; } = String.Empty;
 
+        [JsonPropertyName("imageurl")]
         public string ImageUrl { get; set; } = String.Empty;
 
+        [JsonPropertyName("price")]
         public int Price { get; set; }
 
+        [JsonPropertyName("quantity")]
         public int Quantity { get; set; }
     }
 }
