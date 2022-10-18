5a5dc0ba8b0ad2d3e87e58fb4d8f7b14e7d39849
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-19 01:17:30 +0500

    JsonPropertyName deleted DrugCreateViewModel

--- src/PharmacyAppExam.WebApi/ViewModels/Drugs/DrugCreateViewModel.cs
+++ src/PharmacyAppExam.WebApi/ViewModels/Drugs/DrugCreateViewModel.cs
@@ -7,26 +7,21 @@ namespace PharmacyAppExam.WebApi.ViewModels.Drugs
     {
         [Required(ErrorMessage = "Drug name is required field")]
         [MaxLength(35), MinLength(2)]
-        [JsonPropertyName("name")]
         public string Name { get; set; } = String.Empty;
 
         [Required(ErrorMessage = "Description is required field")]
-        [JsonPropertyName("description")]
         public string Description { get; set; } = String.Empty;
 
 
         [Required(ErrorMessage = "ImageUrl is required field")]
-        [JsonPropertyName("imageUrl")]
         public string ImageUrl { get; set; } = String.Empty;
 
 
         [Required(ErrorMessage = "Price is required field")]
-        [JsonPropertyName("price")]
         public int Price { get; set; }
 
 
         [Required(ErrorMessage = "Quantity is required field")]
-        [JsonPropertyName("quantity")]
         public int Quantity { get; set; }
     }
 }
