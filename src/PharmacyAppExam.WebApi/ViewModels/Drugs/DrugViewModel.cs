ef38ce09d2294701c3da1b86e1c722a84096f8c9
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-18 10:42:24 +0500

    ImageUrl and Description properties added for DrugCreateViewModel and DrugViewModel

--- src/PharmacyAppExam.WebApi/ViewModels/Drugs/DrugViewModel.cs
+++ src/PharmacyAppExam.WebApi/ViewModels/Drugs/DrugViewModel.cs
@@ -6,6 +6,10 @@
 
         public string Name { get; set; } = String.Empty;
 
+        public string Description { get; set; } = String.Empty;
+
+        public string ImageUrl { get; set; } = String.Empty;
+
         public int Price { get; set; }
 
         public int Quantity { get; set; }
