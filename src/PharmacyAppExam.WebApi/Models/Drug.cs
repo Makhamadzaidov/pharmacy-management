8ce027aae1d5c527753227e89757220aadbb890a
Kamronbek Maxamadzoidov <kamron25bek25@gmail.com>
2022-10-18 10:35:39 +0500

    Add ImageUrl, Description for Drug model

--- src/PharmacyAppExam.WebApi/Models/Drug.cs
+++ src/PharmacyAppExam.WebApi/Models/Drug.cs
@@ -8,6 +8,10 @@ namespace PharmacyAppExam.WebApi.Models
         [MaxLength(35)]
         public string Name { get; set; } = String.Empty;
 
+        public string Description { get; set; } = String.Empty;
+
+        public string ImagePath { get; set; } = String.Empty;
+
         public int Price { get; set; }
 
         public int Quantity { get; set; }
